59f1dca09c355aa2e4e3f90d7247dc0c704a53bd
Engine Digital Colombia <85118482+GenserBeltran@users.noreply.github.com>
2023-03-18 20:48:03 +0100

    pendiente hacer pull del local

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        //Pendiente que no ha relaizado PUll de la rama local la remoto
+
     }
 }
