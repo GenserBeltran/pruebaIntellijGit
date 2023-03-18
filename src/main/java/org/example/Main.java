1d0f246ab39ec4567423f943d7c4b1d0904502ad
Daniel Beltran <genserdanie-123@hotmail.com>
2023-03-18 20:38:32 +0100

    Se actualiza el print en main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("EL proyecto en segunda rama tiene 5 versiones");
+
     }
 }
